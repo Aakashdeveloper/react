ca6f31af54443fab53bdf98d6b414a142513654f
AAKASH HANDA <ahanda205@gmail.com>
2016-08-15 23:35:44 +0530

    a

---
+++ app/main.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+var addons = require('react-addons');
+var ReactDOM = require('react-dom');
+ console.log("hello from jsx");
+
+ var GroceryItemList = require('./components/GroceryItemList.jsx');
+
+ ReactDOM.render(<GroceryItemList />, app);

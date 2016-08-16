42205f76ca4344c2c6935236da8dfe8631f2da75
Aakash Handa <aakashh@aakashh1-linux.gur.corp.google.com>
2016-08-16 16:20:36 +0530

    A

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,10 +1,11 @@
 module.exports = function(app){
 	var items =[{
 	name:"ice cream"
+	src:"/img/1.jpg"
     },
      {
 	name:"candy",
-	purchased:true
+	src:"/img/2.jpg"
     }];
     
     app.route('/api/items')

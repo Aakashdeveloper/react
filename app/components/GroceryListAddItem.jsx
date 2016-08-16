44b4ea55001774b215eb44edddf39c3f07392e46
Aakash Handa <aakashh@aakashh1-linux.gur.corp.google.com>
2016-08-16 14:29:55 +0530

    a

--- app/components/GroceryListAddItem.jsx
+++ app/components/GroceryListAddItem.jsx
@@ -2,11 +2,22 @@ var React = require('react');
 var addons = require('react-addons');
 
 module.exports = React.createClass({
+    getInitialState:function(){
+      return {input:""};
+
+    },
+    handleInputName:function(e){
+      this.setState({input: e.target.value});
+    },
+    addItem:function(){
+    e.preventDefault();
+      console.log("Adding items", this.state.input);
+    },
 		render:function(){
 			return(
 				<div className="grocery-addItem">
-					<form>
-					  <input/>
+					<form onSubmit={this.addItem}>
+					  <input value={this.state.input} onChange={this.handleInputName}/>
 					  <button> Add Item </button>
 
 					</form>
